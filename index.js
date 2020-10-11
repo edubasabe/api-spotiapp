58ffbc1293f0fac314c864ce24e926b908780cd1
David Basabe <david.basabe@cencosud.cl>
2020-10-11 17:34:43 -0300

    :zap: enable CORS

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
