2a46f13f24ed2271b54a83b9c063248682759e05
David Basabe <david.basabe@cencosud.cl>
2020-10-11 17:16:45 -0300

    :bug: Change of init command

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const querystring = require('querystring');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
